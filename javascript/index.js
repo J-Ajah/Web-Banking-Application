88f1cee031417177165e5dad617c5f03dd2c645c
J-Ajah <Justice.ajah@amalitech.org>
2021-03-16 09:31:50 +0000

    Added navigation menu with it's responsive styles

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,16 @@
-document.querySelector()
+  let  items = document.querySelector(".searchInput").classList;
+
+ document.querySelector("#search").addEventListener("click", addClassButton)
+
+ function addClassButton(){
+  items.add(".searchInputShow");
+  alert(items)
+} 
+
+
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function myFunction() { 
     let x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
